931a46498c874ec9ee242efce3967bc5027891c7
Scott Hoyt <scottrhoyt@gmail.com>
2017-01-12 21:18:31 -0800

    Basic Carthage Compatibility:
    * Rename SwiftyGif scheme to SwiftyGifExample.
    * Create Framework target.
    * Add shared SwiftyGif scheme to build framework.

---
+++ SwiftyGif/SwiftyGif.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyGif.h
+//  SwiftyGif
+//
+//  Created by Scott Hoyt on 1/12/17.
+//  Copyright © 2017 alexiscreuzot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftyGif.
+FOUNDATION_EXPORT double SwiftyGifVersionNumber;
+
+//! Project version string for SwiftyGif.
+FOUNDATION_EXPORT const unsigned char SwiftyGifVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyGif/PublicHeader.h>
+
+
