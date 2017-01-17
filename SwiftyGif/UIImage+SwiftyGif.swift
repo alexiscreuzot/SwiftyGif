538c3e5288763f7edb388cc954ed121ba7d1c73c
kawolum <kawolum822@gmail.com>
2017-01-16 20:58:18 -0600

    Revert "Not counting 0th frame lose"
    This reverts commit e636fadc16c0005491343eb428065c04fed8ebcf.

--- SwiftyGif/UIImage+SwiftyGif.swift
+++ SwiftyGif/UIImage+SwiftyGif.swift
@@ -199,10 +199,6 @@ public extension UIImage{
                 }
             }
             
-            if(displayPosition[0] == 0){
-                framelosecount += 1
-            }
-            
             if framelosecount <= Float(displayPosition.count) * (1.0 - levelOfIntegrity) ||
                 i == displayRefreshDelayTime.count-1 {
                 
