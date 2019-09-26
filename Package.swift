5e3e83559291a93ec0e613a677c2e322fbc61eb8
Simonas Daniliauskas <daniliauskas.s@gmail.com>
2019-09-26 15:24:06 +0300

    Add SPM support

--- Package.swift
+++ Package.swift
@@ -1,18 +1,22 @@
+// swift-tools-version:5.1
+
 import PackageDescription
 
 let package = Package(
-name: "SwiftyGif",
-exclude: [
-    "SwiftyGifExample"
-],
-targets: [
-    Target(
-        name: "SwiftyGif"
-    ),
-    Target(
-        name: "SwiftyGifTests",
-        dependencies: [
-            .Target(name: "SwiftyGif")
-        ]
-    )
-]
+  name: "SwiftyGif",
+  platforms: [
+    .iOS("8.0"),
+  ],
+  products: [
+    .library(
+      name: "SwiftyGif",
+      targets: ["SwiftyGif"]),
+  ],
+  dependencies: [],
+  targets: [
+    .target(
+      name: "SwiftyGif",
+      dependencies: [],
+      path: "SwiftyGif"),
+  ]
+)
