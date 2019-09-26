19d5675c1f2426c3186e0ce6bfa447272ac56e58
Simonas Daniliauskas <daniliauskas.s@gmail.com>
2019-09-26 15:32:22 +0300

    fix formatting

--- Package.swift
+++ Package.swift
@@ -1,22 +1,22 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.0
 
 import PackageDescription
 
 let package = Package(
-  name: "SwiftyGif",
-  platforms: [
-    .iOS("8.0"),
-  ],
-  products: [
-    .library(
-      name: "SwiftyGif",
-      targets: ["SwiftyGif"]),
-  ],
-  dependencies: [],
-  targets: [
-    .target(
-      name: "SwiftyGif",
-      dependencies: [],
-      path: "SwiftyGif"),
-  ]
+    name: "SwiftyGif",
+    platforms: [
+        .iOS("8.0"),
+    ],
+    products: [
+        .library(
+            name: "SwiftyGif",
+            targets: ["SwiftyGif"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "SwiftyGif",
+            dependencies: [],
+            path: "SwiftyGif"),
+    ]
 )
